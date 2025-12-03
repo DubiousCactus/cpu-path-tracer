dfcc4f4bad78f28456ea723ecc3882e3dc2aa260
Theo Morales <theo.morales.fr@gmail.com>
2025-12-03 14:34:34 +0000

    Refactor Image ops and add Scene as hittable group

--- src/main.zig
+++ src/main.zig
@@ -11,25 +11,27 @@ pub const PPMImage = struct {
     out: *std.Io.Writer,
 
     pub fn init(name: []const u8, width: u16, height: u16) !PPMImage {
-        const file = try std.fs.cwd().createFile(name, .{});
-        var file_buffer: [1024]u8 = undefined;
-        var file_writer = std.fs.File.Writer.init(file, &file_buffer);
-        const out = &file_writer.interface;
-        try out.print("P3\n{d} {d}\n255\n", .{ width, height });
-        return .{
+        var image = PPMImage{
             .width = width,
             .height = height,
-            .file = file,
-            .file_writer = file_writer,
-            .out = out,
+            .file = undefined,
+            .file_writer = undefined,
+            .out = undefined,
         };
+        image.file = try std.fs.cwd().createFile(name, .{});
+        image.file_writer = std.fs.File.Writer.init(image.file, &image.file_buffer);
+        image.out = &image.file_writer.interface;
+        // Header of PPM image: P3 for ASCII colours, then widht height, then max color
+        // value (255).
+        try image.out.print("P3\n{d} {d}\n255\n", .{ width, height });
+        return image;
     }
 
     pub fn close(self: *PPMImage) void {
         self.file.close();
     }
 
-    pub fn write_pixel_buffered(self: *PPMImage, pixel: zm.Vec3) !void {
+    pub fn writePixelBuffered(self: *PPMImage, pixel: zm.Vec3) !void {
         try self.out.print("{d} {d} {d}\n", .{
             @as(u8, @intFromFloat(255.999 * pixel.data[0])),
             @as(u8, @intFromFloat(255.999 * pixel.data[1])),
@@ -70,11 +72,11 @@ pub fn Hits(max_count: comptime_int) type {
         where: [max_count]zm.Vec3,
         normals: [max_count]zm.Vec3,
 
-        pub fn where_slice(self: Self) []zm.Vec3 {
+        pub fn whereSlice(self: Self) []zm.Vec3 {
             return self.where[0..self.count];
         }
 
-        pub fn normal_slice(self: Self) []zm.Vec3 {
+        pub fn normalSlice(self: Self) []zm.Vec3 {
             return self.normals[0..self.count];
         }
     };
@@ -83,15 +85,17 @@ pub fn Hits(max_count: comptime_int) type {
 pub const Hit = struct {
     point: zm.Vec3,
     normal: zm.Vec3,
-    at: f64
+    at: f64,
+    is_front_face: bool,
 };
 
 pub const Hittable = union(enum) {
     sphere: Sphere,
+    hittable_group: HittableGroup,
 
     pub fn hit(self: Hittable, ray: Ray, ray_tmin: f64, ray_tmax: f64) ?Hit {
         switch (self) {
-            inline else => |case| return case.hit(ray, ray_tmin, ray_tmax),
+            inline else => |impl| return impl.hit(ray, ray_tmin, ray_tmax),
         }
     }
 };
@@ -117,28 +121,23 @@ pub const Sphere = struct {
             }
         }
         const p = ray.at(root);
+        const outward_normal = p.sub(self.origin).scale(1 / self.radius);
+        const is_front_face = ray.dir.dot(outward_normal) <= 0;
         return .{
             .point = p,
-            .normal = p.sub(self.origin).scale(1 / self.radius),
+            .normal = if (is_front_face) outward_normal else outward_normal.scale(-1),
             .at = root,
+            .is_front_face = is_front_face,
         };
     }
 
-    pub fn normal_at(self: Sphere, p: zm.Vec3) zm.Vec3 {
+    pub fn normalAt(self: Sphere, p: zm.Vec3) zm.Vec3 {
         return p.sub(self.origin).norm();
     }
 };
 
-pub fn write_pixel(writer: *std.io.Writer, pixel: zm.Vec3) !void {
-    try writer.print("{d} {d} {d}\n", .{
-        @as(u8, @intFromFloat(255.999 * pixel.data[0])),
-        @as(u8, @intFromFloat(255.999 * pixel.data[1])),
-        @as(u8, @intFromFloat(255.999 * pixel.data[2])),
-    });
-}
-
-pub fn ray_color(object: Hittable, ray: Ray) zm.Vec3 {
-    if (object.hit(ray, 0.1, 100.0)) |hit| {
+pub fn rayColor(object: Hittable, ray: Ray) zm.Vec3 {
+    if (object.hit(ray, 0.001, 1000.0)) |hit| {
         return hit.normal.add(zm.Vec3{ .data = .{ 1, 1, 1 } }).scale(0.5);
     }
     return zm.Vec3.lerp(
@@ -147,10 +146,41 @@ pub fn ray_color(object: Hittable, ray: Ray) zm.Vec3 {
         0.5 * (ray.dir.data[1] + 1.0),
     );
 }
+
+pub const HittableGroup = struct {
+    objects: std.ArrayList(Hittable) = std.ArrayList(Hittable).empty,
+
+    pub fn addOne(self: *HittableGroup, object: Hittable, gpa: std.mem.Allocator) !void {
+        try self.objects.append(gpa, object);
+    }
+
+    pub fn deinit(self: *HittableGroup, gpa: std.mem.Allocator) void {
+        self.objects.deinit(gpa);
+    }
+
+    pub fn hit(self: HittableGroup, ray: Ray, ray_tmin: f64, ray_tmax: f64) ?Hit {
+        var last_hit: ?Hit = null;
+        var closest_so_far = ray_tmax;
+
+        for (self.objects.items) |obj| {
+            if (obj.hit(ray, ray_tmin, closest_so_far)) |current_hit| {
+                last_hit = current_hit;
+                closest_so_far = current_hit.at;
+            }
+        }
+
+        return last_hit;
+    }
+};
+
 pub fn main() !void { // Image
     const width: u16 = 640;
     const aspect_ratio: f32 = 16.0 / 9.0;
     const height = @max(1, @as(u16, @intFromFloat(@as(f32, @floatFromInt(width)) / aspect_ratio)));
+
+    var gpa = std.heap.DebugAllocator(.{}).init;
+    const allocator = gpa.allocator();
+    // defer gpa.deinit();
 
     // Camera
     const viewport_height: f32 = 2.0;
@@ -173,39 +203,31 @@ pub fn main() !void { // Image
     const pixel00_loc: zm.Vec3 = pixel_delta_u.add(pixel_delta_v).scale(0.5).add(viewport_upper_left);
 
     // File output
-    const file = try std.fs.cwd().createFile("image.ppm", .{});
-    defer file.close();
-    var file_buffer: [1024]u8 = undefined;
-    var file_writer = std.fs.File.Writer.init(file, &file_buffer);
-    const out = &file_writer.interface;
-
-    // var image = try PPMImage.init("image.ppm", 256, 256);
-    // defer image.close();
+    var image = try PPMImage.init("image.ppm", width, height);
+    defer image.close();
 
     // Progress bar
     var root_progress = std.Progress.start(
         .{ .estimated_total_items = height, .root_name = "Rendering lines..." },
     );
 
-    // Header of PPM image: P3 for ASCII colours, then widht height, then max color
-    // value (255).
-    try out.print("P3\n{d} {d}\n255\n", .{ width, height });
-
     // Rendering!
     const sphere = Hittable{ .sphere = Sphere{ .radius = 0.5, .origin = zm.Vec3{ .data = .{ 0, 0, -1 } } } };
+    const pseudo_plane = Hittable{ .sphere = Sphere{ .radius = 100, .origin = zm.Vec3{ .data = .{ 0, -100.5, -1 } } } };
+    var scene = Hittable{ .hittable_group = HittableGroup{} };
+    defer scene.hittable_group.deinit(allocator);
+    try scene.hittable_group.addOne(sphere, allocator);
+    try scene.hittable_group.addOne(pseudo_plane, allocator);
     for (0..height) |j| {
         for (0..width) |i| {
-            // const r_f = @as(f64, @floatFromInt(i)) / @as(f64, @floatFromInt(width - 1));
-            // const g_f = @as(f64, @floatFromInt(j)) / @as(f64, @floatFromInt(height - 1));
-            // const b_f = 0;
             const pixel_center = pixel00_loc.add(pixel_delta_u.scale(@floatFromInt(i))).add(pixel_delta_v.scale(@floatFromInt(j)));
             const ray_direction = pixel_center.sub(cam_eye);
             const ray = Ray.init(cam_eye, ray_direction);
-            const c = ray_color(sphere, ray);
-            try write_pixel(out, c);
+            const c = rayColor(scene, ray);
+            try image.writePixelBuffered(c);
         }
-        try out.flush();
+        try image.flush();
         root_progress.completeOne();
     }
-    try out.flush();
+    try image.flush();
 }
