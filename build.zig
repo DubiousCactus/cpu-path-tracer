4f504c53d35fd59b9c5906b786d495356c73e705
Theo Morales <theo.morales.fr@gmail.com>
2026-01-06 17:24:34 +0000

    Fix viewer in optimized release mode build for x86_64

--- build.zig
+++ build.zig
@@ -44,6 +44,7 @@ pub fn build(b: *std.Build) void {
     const raylib_dep = b.dependency("raylib_zig", .{
         .target = target,
         .optimize = optimize,
+        .linkage = .dynamic,
     });
 
     const raylib = raylib_dep.module("raylib"); // main raylib module
